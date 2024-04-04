ca97773bb27514bcceb344bb293d56bf688ec6ad
Satoki Hata <satokihata@gmail.com>
2024-04-04 21:19:03 +0900

    ui

--- app/bbs-posts/[id]/page.tsx
+++ app/bbs-posts/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { BBSData } from "@/app/types/types";
+import Link from "next/link";
 import React from "react";
 
 async function getDetailBBSData(id: number) {
@@ -12,8 +13,24 @@ async function getDetailBBSData(id: number) {
 
 const BBSDetailPage = async ({ params }: { params: { id: number } }) => {
   const bbsDetailData = await getDetailBBSData(params.id);
-  console.log(bbsDetailData);
-  return <div>BBSDetailPage</div>;
+  const { title, content, username } = bbsDetailData;
+  return (
+    <div className="mx-auto max-w-4xl p-4">
+      <div className="mb-8">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-gray-700">{username}</p>
+      </div>
+      <div className="mb-8">
+        <p className="text-gray-900">{content}</p>
+      </div>
+      <Link
+        href={"/"}
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md"
+      >
+        戻る
+      </Link>
+    </div>
+  );
 };
 
 export default BBSDetailPage;
