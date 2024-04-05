17a7ed0d07b0a3d62d2c9633bd41a083d66aa0c3
Satoki Hata <satokihata@gmail.com>
2024-04-05 10:03:08 +0900

    post api

--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { POST } from "@/app/api/post/route";
 import { Button } from "@/app/components/BBSForm/button";
 import {
   Form,
@@ -9,9 +12,46 @@ import {
   FormMessage,
 } from "@/app/components/BBSForm/form";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-const page = () => {
+const formSchema = z.object({
+  username: z.string().min(2, { message: "ユーザー名は2文字以上" }),
+  title: z.string().min(2, { message: "タイトルは2文字以上" }),
+  content: z
+    .string()
+    .min(10, { message: "本文は2文字以上" })
+    .max(140, { message: "本文は140文字以内" }),
+});
+
+const CreateBBSPage = () => {
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  async function onSubmit(value: z.infer<typeof formSchema>) {
+    const { username, title, content } = value;
+    try {
+      await fetch("http://localhost:3000/api/post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, title, content }),
+      });
+      router.push("/");
+    } catch (error) {}
+  }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -22,11 +62,35 @@ const page = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="username" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>title</FormLabel>
+              <FormControl>
+                <Input placeholder="title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>content</FormLabel>
+              <FormControl>
+                <Input placeholder="content" {...field} />
+              </FormControl>
+
               <FormMessage />
             </FormItem>
           )}
@@ -37,4 +101,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default CreateBBSPage;
