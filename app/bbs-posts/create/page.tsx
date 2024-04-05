d23b792018d37eb0c03079a79707b647f32c719f
Satoki Hata <satokihata@gmail.com>
2024-04-05 10:49:39 +0900

    done

--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { POST } from "@/app/api/post/route";
+import { postBBS } from "@/app/actions/postBBSAction";
 import { Button } from "@/app/components/BBSForm/button";
 import {
   Form,
@@ -13,12 +13,12 @@ import {
 } from "@/app/components/BBSForm/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const formSchema = z.object({
+export const formSchema = z.object({
   username: z.string().min(2, { message: "ユーザー名は2文字以上" }),
   title: z.string().min(2, { message: "タイトルは2文字以上" }),
   content: z
@@ -40,16 +40,11 @@ const CreateBBSPage = () => {
 
   async function onSubmit(value: z.infer<typeof formSchema>) {
     const { username, title, content } = value;
-    try {
-      await fetch("http://localhost:3000/api/post", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, title, content }),
-      });
-      router.push("/");
-    } catch (error) {}
+    postBBS({
+      username,
+      title,
+      content,
+    });
   }
 
   return (
